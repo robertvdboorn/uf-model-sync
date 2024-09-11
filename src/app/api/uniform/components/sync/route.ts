6d02ad95f7247ef86bd733064fe46ccdce842761
Robert van den Boorn <robertb@uniform.dev>
2024-09-11 17:14:15 +0200

    feat: added backup generation upon syncing

--- src/app/api/uniform/components/sync/route.ts
+++ src/app/api/uniform/components/sync/route.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import Uniform from "@/lib/uniform";
+import fs from "fs/promises";
+import path from "path";
+
+const createBackupFilePath = (componentId: string, projectId: string) => {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Format timestamp safely for file name
+  return path.join(process.cwd(), 'backup', `${componentId}_${projectId}_${timestamp}.json`);
+};
 
 export async function POST(req: NextRequest) {
   try {
     const {
       source: { projectId: sourceProjectId, apiKey: sourceApiKey },
-      destination: {
-        projectId: destinationProjectId,
-        apiKey: destinationApiKey,
-      },
+      destination: { projectId: destinationProjectId, apiKey: destinationApiKey },
       componentId,
     } = await req.json();
 
@@ -22,21 +26,31 @@ export async function POST(req: NextRequest) {
       apiKey: destinationApiKey,
     });
 
-    const component = await sourceClient.getComponentDefinitions({
-      componentId,
-    });
+    const destinationComponent = await destinationClient.getComponentDefinitions({ componentId });
 
-    if (component === null || component.componentDefinitions.length === 0) {
+    if (destinationComponent && destinationComponent.componentDefinitions.length > 0) {
+      const { componentDefinitions } = destinationComponent;
+
+      const backupFilePath = createBackupFilePath(componentId, destinationProjectId);
+      await fs.mkdir(path.dirname(backupFilePath), { recursive: true }); // Ensure /backup folder exists
+      await fs.writeFile(backupFilePath, JSON.stringify(componentDefinitions[0], null, 2));
+
+      console.log(`Backup saved: ${backupFilePath}`);
+    }
+
+    const sourceComponent = await sourceClient.getComponentDefinitions({ componentId });
+
+    if (sourceComponent === null || sourceComponent.componentDefinitions.length === 0) {
       return NextResponse.json(
         { error: "Component not found in source project" },
         { status: 400 }
       );
     }
 
-    const { componentDefinitions } = component;
+    const { componentDefinitions: sourceComponentDefinitions } = sourceComponent;
 
     await destinationClient.updateComponentDefinition({
-      componentDefinition: componentDefinitions[0],
+      componentDefinition: sourceComponentDefinitions[0],
     });
 
     return NextResponse.json({ message: "Success" });
